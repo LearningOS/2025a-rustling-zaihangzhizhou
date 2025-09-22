737fb988045f50e64e74d2ec69c259d5fd17cba9
zaihangzhizhou <13858632985@163.com>
2025-09-22 20:12:27 +0800

    update

--- exercises/algorithm/algorithm4.rs
+++ exercises/algorithm/algorithm4.rs
@@ -3,7 +3,6 @@
 	This problem requires you to implement a basic interface for a binary tree
 */
 
-//I AM NOT DONE
 use std::cmp::Ordering;
 use std::fmt::Debug;
 
@@ -51,12 +50,26 @@ where
     // Insert a value into the BST
     fn insert(&mut self, value: T) {
         //TODO
+        match self.root{
+            Some(ref mut root)=>root.insert(value),
+            None=>self.root=Some(Box::new(TreeNode::new(value)))
+        }
     }
 
     // Search for a value in the BST
     fn search(&self, value: T) -> bool {
         //TODO
-        true
+        let mut current=&self.root;
+        while let Some(node)=current{
+            if value<node.value{
+                current=&node.left;
+            }else if value>node.value{
+                current=&node.right;
+            }else{
+                return true;
+            }
+        }
+        false
     }
 }
 
@@ -67,6 +80,17 @@ where
     // Insert a node into the tree
     fn insert(&mut self, value: T) {
         //TODO
+        if value<self.value{
+            match self.left{
+                Some(ref mut left)=>left.insert(value),
+                None=>self.left=Some(Box::new(TreeNode::new(value)))
+            }
+        }else if value>self.value{
+            match self.right{
+                Some(ref mut right)=>right.insert(value),
+                None=>self.right=Some(Box::new(TreeNode::new(value)))
+            }
+        }
     }
 }
 
--- exercises/algorithm/algorithm5.rs
+++ exercises/algorithm/algorithm5.rs
@@ -3,7 +3,6 @@
 	This problem requires you to implement a basic BFS algorithm
 */
 
-//I AM NOT DONE
 use std::collections::VecDeque;
 
 // Define a graph
@@ -27,10 +26,18 @@ impl Graph {
 
     // Perform a breadth-first search on the graph, return the order of visited nodes
     fn bfs_with_return(&self, start: usize) -> Vec<usize> {
-        
-		//TODO
-
         let mut visit_order = vec![];
+        let mut deque=VecDeque::new();
+        deque.push_back(start);
+        while let Some(node)=deque.pop_front(){
+            if visit_order.contains(&node){
+                continue;
+            }
+            visit_order.push(node.clone());
+            for item in &self.adj[node]{
+                deque.push_back(item.clone());
+            }
+        }
         visit_order
     }
 }
