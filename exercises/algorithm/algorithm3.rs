9e4241119e3bf38d1de6b3f6b915b8d2136e3f0a
zaihangzhizhou <13858632985@163.com>
2025-09-22 17:07:27 +0800

    update

--- exercises/algorithm/algorithm3.rs
+++ exercises/algorithm/algorithm3.rs
@@ -3,10 +3,20 @@
 	This problem requires you to implement a sorting algorithm
 	you can use bubble sorting, insertion sorting, heap sorting, etc.
 */
-// I AM NOT DONE
 
-fn sort<T>(array: &mut [T]){
-	//TODO
+fn sort<T>(array: &mut [T])
+where
+    T:PartialOrd
+{
+	for i in 0..array.len()-1{
+        let mut index=i;
+        for j in i+1..array.len(){
+            if array[j]<array[index]{
+                index=j;
+            }
+        }
+        array.swap(index,i);
+    }
 }
 #[cfg(test)]
 mod tests {
